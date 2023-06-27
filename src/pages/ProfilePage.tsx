a32392b210fa9e0b76f0df12528733575b58c037
cmyui <cmyuiosu@gmail.com>
2023-06-27 00:10:53 -0400

    fetch scores & some progress on profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -2,10 +2,35 @@ import { useParams } from "react-router-dom";
 import { Typography, Paper, Divider, List } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import { fetchManyScores } from "../adapters/scores";
+import { Score } from "../interfaces/scores";
+import { Failure, Success } from "../interfaces/api";
 
 export const ProfilePage = () => {
+  const [data, setData] = useState<Score[] | null>(null);
+  const [isLoading, setLoading] = useState(false); // is this a reason to sep from data!=null?
+  const [error, setError] = useState("");
   const { accountId } = useParams();
-  const userpageContent = "Hello World!"; // TODO: dynamic
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const allStats = await fetchManyScores({
+        page: 1,
+        pageSize: 50,
+        sortBy: "performance_points",
+      });
+      if (allStats.status === "error") {
+        setError("Failed to fetch data from server");
+        return;
+      }
+
+      setData(allStats.data);
+    };
+
+    // run this asynchronously
+    fetchData().catch(console.error);
+  }, []);
 
   if (!accountId) {
     return (
@@ -16,6 +41,7 @@ export const ProfilePage = () => {
       </>
     );
   }
+  const userpageContent = "Hello World!"; // TODO: dynamic
 
   return (
     <>
@@ -47,21 +73,36 @@ export const ProfilePage = () => {
         <Stack
           direction="row"
           spacing={2}
-          sx={{ justifyContent: "space-between" }}
+          sx={{ justifyContent: "space-evenly" }}
         >
-          <Box>
-            <Paper elevation={3} sx={{ p: 2, height: "12rem" }}>
+          <Box sx={{ width: 3 / 5 }}>
+            <Paper elevation={3} sx={{ height: 1 / 1 }}>
               {/* Ranking Graph */}
-              <Stack direction="column">
+              <Stack direction="column" sx={{ p: 2 }}>
                 <Typography>Gaming</Typography>
                 <Typography>Gaming</Typography>
               </Stack>
             </Paper>
           </Box>
-          <Box>
-            <Paper elevation={3} sx={{ p: 2 }}>
+          <Box sx={{ width: 2 / 5 }}>
+            <Paper elevation={3}>
               {/* Overall Stats */}
-              <Typography>Gaming</Typography>
+              <Stack direction="column" sx={{ p: 2 }}>
+                {/* TODO: this should be a table */}
+                <Typography>Total Score</Typography>
+                <Typography>Ranked Score</Typography>
+                <Typography>Performance Points</Typography>
+                <Typography>Play Count</Typography>
+                <Typography>Play Time</Typography>
+                <Typography>Accuracy</Typography>
+                <Typography>Highest Combo</Typography>
+                <Typography>Total Hits</Typography>
+                <Typography>SS Count (Hidden)</Typography>
+                <Typography>SS Count (No Hidden)</Typography>
+                <Typography>S Count (Hidden)</Typography>
+                <Typography>S Count (No Hidden)</Typography>
+                <Typography>A Count</Typography>
+              </Stack>
             </Paper>
           </Box>
         </Stack>
