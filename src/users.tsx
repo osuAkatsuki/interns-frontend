e6e2002ec0bcd972e3ef591566b84d072bad97c0
cmyui <cmyuiosu@gmail.com>
2023-06-27 21:35:01 -0400

    test commit

--- src/users.tsx
+++ src/users.tsx
@@ -1,76 +1,76 @@
-import React from "react";
-import type { Session } from "./interfaces/sessions";
-import type { Account } from "./interfaces/accounts";
+// import React from "react";
+// import type { Session } from "./interfaces/sessions";
+// import type { Account } from "./interfaces/accounts";
 
-interface User {
-  session: Session;
-  account: Account;
-}
+// interface User {
+//   session: Session;
+//   account: Account;
+// }
 
-export type UserContextType = {
-  user: User | null;
-  setUser: (user: User | null) => void;
-};
+// export type UserContextType = {
+//   user: User | null;
+//   setUser: (user: User | null) => void;
+// };
 
-export const UserContext = React.createContext<UserContextType | undefined>(
-  undefined
-);
+// export const UserContext = React.createContext<UserContextType | undefined>(
+//   undefined
+// );
 
-export const useUserContext = () => {
-  const userContext = React.useContext(UserContext);
-  if (userContext === undefined) {
-    throw new Error("useUserContext must be inside a UserContextProvider");
-  }
-  return userContext;
-};
+// export const useUserContext = () => {
+//   const userContext = React.useContext(UserContext);
+//   if (userContext === undefined) {
+//     throw new Error("useUserContext must be inside a UserContextProvider");
+//   }
+//   return userContext;
+// };
 
-interface Props {
-  children: React.ReactNode;
-}
+// interface Props {
+//   children: React.ReactNode;
+// }
 
-// TODO: store user data in cookies as opposed to local storage
+// // TODO: store user data in cookies as opposed to local storage
 
-export const setUserInLocalStorage = (user: User) => {
-  localStorage.setItem("user", JSON.stringify(user));
-};
+// export const setUserInLocalStorage = (user: User) => {
+//   localStorage.setItem("user", JSON.stringify(user));
+// };
 
-export const getUserFromLocalStorage = () => {
-  const cached = localStorage.getItem("user");
-  if (cached) {
-    const parsed = JSON.parse(cached) as User;
-    if (new Date() >= parsed.session.expiresAt) {
-      // value in local storage has expired
-      removeUserFromLocalStorage();
-      return null;
-    }
-    return parsed;
-  }
-  return null;
-};
+// export const getUserFromLocalStorage = () => {
+//   const cached = localStorage.getItem("user");
+//   if (cached) {
+//     const parsed = JSON.parse(cached) as User;
+//     if (new Date() >= parsed.session.expiresAt) {
+//       // value in local storage has expired
+//       removeUserFromLocalStorage();
+//       return null;
+//     }
+//     return parsed;
+//   }
+//   return null;
+// };
 
-export const removeUserFromLocalStorage = () => {
-  localStorage.removeItem("user");
-};
+// export const removeUserFromLocalStorage = () => {
+//   localStorage.removeItem("user");
+// };
 
-export const UserContextProvider: React.FC<Props> = ({ children }) => {
-  const [user, setUser] = React.useState<User | null>(
-    getUserFromLocalStorage()
-  );
-  return (
-    <UserContext.Provider
-      value={{
-        user,
-        setUser: (value: React.SetStateAction<User | null>) => {
-          if (value) {
-            setUserInLocalStorage(value as User);
-          } else {
-            removeUserFromLocalStorage();
-          }
-          setUser(value);
-        },
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
-};
+// export const UserContextProvider: React.FC<Props> = ({ children }) => {
+//   const [user, setUser] = React.useState<User | null>(
+//     getUserFromLocalStorage()
+//   );
+//   return (
+//     <UserContext.Provider
+//       value={{
+//         user,
+//         setUser: (value: React.SetStateAction<User | null>) => {
+//           if (value) {
+//             setUserInLocalStorage(value as User);
+//           } else {
+//             removeUserFromLocalStorage();
+//           }
+//           setUser(value);
+//         },
+//       }}
+//     >
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
