1f744b5c8f949cb5bcbbdd15481627a954efc3cd
cmyui <cmyuiosu@gmail.com>
2023-07-04 03:09:03 -0400

    make it clear when gamemode buttons are not selected

--- src/components/GameModeSelectionBar.tsx
+++ src/components/GameModeSelectionBar.tsx
@@ -17,28 +17,28 @@ export const GameModeSelectionBar = ({
       <Stack direction="row" justifyContent="space-between" spacing={1}>
         <Stack direction="row" spacing={1}>
           <Button
-            variant="contained"
+            variant={gameMode === ClientGameMode.Standard ? "contained" : "outlined"}
             disabled={!isRealGameMode(ClientGameMode.Standard, relaxMode)}
             onClick={() => setGameMode(ClientGameMode.Standard)}
           >
             Standard
           </Button>
           <Button
-            variant="contained"
+            variant={gameMode === ClientGameMode.Taiko ? "contained" : "outlined"}
             disabled={!isRealGameMode(ClientGameMode.Taiko, relaxMode)}
             onClick={() => setGameMode(ClientGameMode.Taiko)}
           >
             Taiko
           </Button>
           <Button
-            variant="contained"
+            variant={gameMode === ClientGameMode.Catch ? "contained" : "outlined"}
             disabled={!isRealGameMode(ClientGameMode.Catch, relaxMode)}
             onClick={() => setGameMode(ClientGameMode.Catch)}
           >
             Catch The Beat
           </Button>
           <Button
-            variant="contained"
+            variant={gameMode === ClientGameMode.Mania ? "contained" : "outlined"}
             disabled={!isRealGameMode(ClientGameMode.Mania, relaxMode)}
             onClick={() => setGameMode(ClientGameMode.Mania)}
           >
@@ -47,21 +47,21 @@ export const GameModeSelectionBar = ({
         </Stack>
         <Stack direction="row" spacing={1}>
           <Button
-            variant="contained"
+            variant={relaxMode === RelaxMode.Vanilla ? "contained" : "outlined"}
             disabled={!isRealGameMode(gameMode, RelaxMode.Vanilla)}
             onClick={() => setRelaxMode(RelaxMode.Vanilla)}
           >
             Vanilla
           </Button>
           <Button
-            variant="contained"
+            variant={relaxMode === RelaxMode.Relax ? "contained" : "outlined"}
             disabled={!isRealGameMode(gameMode, RelaxMode.Relax)}
             onClick={() => setRelaxMode(RelaxMode.Relax)}
           >
             Relax
           </Button>
           <Button
-            variant="contained"
+            variant={relaxMode === RelaxMode.Autopilot ? "contained" : "outlined"}
             disabled={!isRealGameMode(gameMode, RelaxMode.Autopilot)}
             onClick={() => setRelaxMode(RelaxMode.Autopilot)}
           >
